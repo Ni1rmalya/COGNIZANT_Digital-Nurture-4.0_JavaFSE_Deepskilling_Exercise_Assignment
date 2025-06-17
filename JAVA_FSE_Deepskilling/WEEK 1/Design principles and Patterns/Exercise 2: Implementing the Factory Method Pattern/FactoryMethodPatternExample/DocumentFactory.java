8dee1e4f954ceb0759090767ee401ad3bb718fed
Ni1rmalya <113786697+Ni1rmalya@users.noreply.github.com>
2025-06-18 01:32:19 +0530

    Create DocumentFactory.java

---
+++ JAVA_FSE_Deepskilling/WEEK 1/Design principles and Patterns/Exercise 2: Implementing the Factory Method Pattern/FactoryMethodPatternExample/DocumentFactory.java
@@ -0,0 +1,24 @@
+public abstract class DocumentFactory {
+    public abstract Document createDocument();
+}
+
+class WordFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new WordDocument();
+    }
+}
+
+class PdfFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new PdfDocument();
+    }
+}
+
+class ExcelFactory extends DocumentFactory {
+    @Override
+    public Document createDocument() {
+        return new ExcelDocument();
+    }
+}
