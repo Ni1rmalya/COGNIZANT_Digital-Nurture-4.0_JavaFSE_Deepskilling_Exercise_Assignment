ada232b5b268920ed8cd01d308f7a39d30bda10a
Ni1rmalya <113786697+Ni1rmalya@users.noreply.github.com>
2025-06-17 13:03:26 +0530

    Create Main.java

---
+++ JAVA_FSE_Deepskilling/WEEK 1/Design principles and Patterns/Exercise 1: Implementing the Singleton Pattern/SingletonPatternExample/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("Logging from logger1.");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Logging from logger2.");
+
+        // Check if both references point to the same instance
+        if (logger1 == logger2) {
+            System.out.println("Both loggers are the same instance (Singleton confirmed).");
+        } else {
+            System.out.println("Different instances (Singleton failed).");
+        }
+    }
+}
