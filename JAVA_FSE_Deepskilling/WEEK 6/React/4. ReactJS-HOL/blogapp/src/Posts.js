de5261d6498b7b356ca392d9318c33df30c68de2
NIRMALYA CHAKRABORTY <113786697+Ni1rmalya@users.noreply.github.com>
2025-07-25 10:52:05 +0530

    Add files via upload

---
+++ JAVA_FSE_Deepskilling/WEEK 6/React/4. ReactJS-HOL/blogapp/src/Posts.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import Post from "./Post";
+
+class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      hasError: false
+    };
+  }
+
+  loadPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(response => response.json())
+      .then(data => {
+        const postList = data.map(p => new Post(p.userId, p.id, p.title, p.body));
+        this.setState({ posts: postList });
+      })
+      .catch(error => {
+        console.error("Error loading posts:", error);
+      });
+  }
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    alert("An error occurred: " + error.message);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+
+    return (
+      <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Posts;
